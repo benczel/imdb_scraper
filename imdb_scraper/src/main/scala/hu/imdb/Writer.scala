f295945b32fd78469be1c714dc448057adac825c
benczel <benczel@benczels-MacBook-Pro.local>
2023-07-11 10:05:04 +0200

    added comment

--- imdb_scraper/src/main/scala/hu/imdb/Writer.scala
+++ imdb_scraper/src/main/scala/hu/imdb/Writer.scala
@@ -5,23 +5,43 @@ import com.github.tototoshi.csv.CSVWriter
 
 import java.io.File
 
+/***
+ * Writer class for writing the csv out
+ */
 class Writer {
 
-  private def createCSVWriter(file: File): CSVWriter = {
+  /***
+   * Open the given file for writing
+   *
+   * @param file a file object that represents the output file
+   * @return an opened CSVWriter object
+   */
+  private def openTheFile(file: File): CSVWriter = {
     CSVWriter.open(file)
   }
 
-
+  /*** Write the given dataset into the given file
+   *
+   * @param fileName where to be required the dataset
+   * @param dataSet that contains the data and to be wanted to write
+   */
   def write(fileName: String,
             dataSet: List[List[Any]]
            ):Unit = {
-    val csvWriter = createCSVWriter(new File(fileName))
+    val csvWriter = openTheFile(new File(fileName))
     csvWriter.writeAll(dataSet)
     csvWriter.close()
   }
 }
 
+/***
+ * Factory for [[Writer]] instances
+ */
 object Writer{
-
+  /**
+   * Creates a new writer instance
+   *
+   * @return a new Writer object
+   */
   def apply():Writer = new Writer()
 }
